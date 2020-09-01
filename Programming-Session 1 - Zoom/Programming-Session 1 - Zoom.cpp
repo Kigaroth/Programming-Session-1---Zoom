0512b82b793174f6f8f8758862876c9a5bede03c
Kigaroth <hp_20@hotmail.com>
2020-09-01 13:28:11 +0200

    Enter Full Name

--- Programming-Session 1 - Zoom/Programming-Session 1 - Zoom.cpp
+++ Programming-Session 1 - Zoom/Programming-Session 1 - Zoom.cpp
@@ -5,16 +5,20 @@ This is my first code. It was made in a Zoom session.
 #include <iostream>
 #include <string>
 
-int age;
+int age = 21;
 bool sw = false;
-char Intial = 'H';
-std::string name = "Hans-Petter";
+char Intial = 'h';
+std::string Fullname = "";
 double pi = 3.14;
 
 int main() {
 	
-	std::cout << "Enter your age : ";
-	std::cin >> age;
-	std::cout << "Your age is : " << age <<std::endl; // "\n" = std::endl;  \t
-	std::cout << "You are too young. ";
+	std::cout << "Enter your full name : ";
+	std::getline(std::cin, Fullname);
+	std::cout << "Your full name is: " << Fullname;
+	//First try
+	//std::cout << "Enter your age : ";
+	//std::cin >> age;
+	//std::cout << "Your age is : " << age <<std::endl; // "\n" = std::endl;  \t
+	//std::cout << "You are too young. ";
 }
