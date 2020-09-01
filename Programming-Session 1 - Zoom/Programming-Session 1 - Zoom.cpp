19444c8f2087ef5dee18282599211eeea971c25a
Kigaroth <hp_20@hotmail.com>
2020-09-01 13:20:45 +0200

    Made some changes

--- Programming-Session 1 - Zoom/Programming-Session 1 - Zoom.cpp
+++ Programming-Session 1 - Zoom/Programming-Session 1 - Zoom.cpp
@@ -1,6 +1,20 @@
-#include <iostream>
+/*
+This is my first code. It was made in a Zoom session.
+*/
 
-int main()
-{
-    std::cout << "Hello World!\n";
+#include <iostream>
+#include <string>
+
+int age;
+bool sw = false;
+char Intial = 'H';
+std::string name = "Hans-Petter";
+double pi = 3.14;
+
+int main() {
+	
+	std::cout << "Enter your age : ";
+	std::cin >> age;
+	std::cout << "Your age is : " << age <<std::endl; // "\n" = std::endl;  \t
+	std::cout << "You are too young. ";
 }
