a46ee3fdcca73c104c6fec1b4a9d904d95816913
Kigaroth <hp_20@hotmail.com>
2020-09-01 13:57:25 +0200

    More Changes

--- Programming-Session 1 - Zoom/Programming-Session 1 - Zoom.cpp
+++ Programming-Session 1 - Zoom/Programming-Session 1 - Zoom.cpp
@@ -4,19 +4,45 @@ This is my first code. It was made in a Zoom session.
 
 #include <iostream>
 #include <string>
+#include <conio.h>
 
 int age = 21;
 bool sw = false;
 char Intial = 'h';
 std::string Fullname = "";
 double pi = 3.14;
+int a = 10;
+int b = 5;
+int c = 20;
+int z = 10;
 
 int main() {
-	
+	system("cls");
+	system("pause");
+
+	if (a > b) {
+		std::cout << "A is greater than B\n";
+	}
+	else {
+		std::cout << "B is greater than A\n";
+	}
+	if (a == z) {
+		std::cout << "A and Z are equal\n";
+	}
+	if (a < b) { // if (a > z || a == z))
+		std::cout << "Nothing happens here\n";
+	}
+	if (a != b) {
+		std::cout << "A and B are not equal\n";
+	}
+
 	std::cout << "Enter your full name : ";
 	std::getline(std::cin, Fullname);
 	std::cout << "Your full name is: " << Fullname;
-	//First try
+	_getch();
+	system("pause");
+
+	//First attempt - Enter your age
 	//std::cout << "Enter your age : ";
 	//std::cin >> age;
 	//std::cout << "Your age is : " << age <<std::endl; // "\n" = std::endl;  \t
